a85128db4e9ebee077391f541a63a143d2269520
adiucas <adi@ucabd.com>
2022-06-23 15:21:09 +0600

    fewqfwe

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 $('.first-option').hide();
+
+var $grid = $('.grid').isotope({
+  // options
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows'
+});
+
+$('.filter button').on("click", function(){
+	var value = $(this).attr('data-name');
+	$grid.isotope({
+		filter:value
+	});
+})
--- js/main.js
+++ js/main.js
@@ -31,3 +31,20 @@ $('.first-option').hide();
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll > 60) {
+      $(".navbar").addClass("bg-nav")
+      $(".navbar").css("box-shadow" , "0 3px 3px #e91b2321");
+      $(".navbar-light .navbar-nav .nav-link").addClass("sticky-nav-link");
+      $(".navbar-light .navbar-nav .active>.nav-link").removeClass("sticky-nav-link");
+    }
+    else{
+      $(".navbar").removeClass("bg-nav")
+      $(".navbar-light .navbar-nav .nav-link").removeClass("sticky-nav-link");
+      $(".navbar").css("box-shadow" , "unset");
+    }
+  })
+})
