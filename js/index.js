28a844baec3355b1f210d7ec72b901824cc12fe2
adiucas <adi@ucabd.com>
2022-06-28 11:55:49 +0600

    wefwefwe

--- js/index.js
+++ js/index.js
@@ -12,3 +12,20 @@ $('.filter button').on("click", function(){
 		filter:value
 	});
 })
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll > 60) {
+      $(".navbar").addClass("bg-nav")
+      $(".navbar").css("box-shadow" , "0 3px 3px #e91b2321");
+      $(".navbar-light .navbar-nav .nav-link").addClass("sticky-nav-link");
+      $(".navbar-light .navbar-nav .active>.nav-link").removeClass("sticky-nav-link");
+    }
+    else{
+      $(".navbar").removeClass("bg-nav")
+      $(".navbar-light .navbar-nav .nav-link").removeClass("sticky-nav-link");
+      $(".navbar").css("box-shadow" , "unset");
+    }
+  })
+})
